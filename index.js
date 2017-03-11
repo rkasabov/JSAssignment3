656a4206d9c4a561c0ccf751a4dafc03bf3a5bc5
Rumen Kolev <Rumen@rumens-macbook-pro-2.local>
2017-03-10 23:23:04 -0700

    Partially completed

--- index.js
+++ index.js
@@ -10,9 +10,73 @@ http.listen( port, function () {
 
 app.use(express.static(__dirname + '/public'));
 
+  //Users currently connected to the chat system
+  var chatUsers = [];
+
 // listen to 'chat' messages
 io.on('connection', function(socket){
-    socket.on('chat', function(msg){
-	io.emit('chat', msg);
-    });
+
+  //Assign a username
+  let currUsername = generateUsername();
+  let existingUser = chatUsers.indexOf(currUsername);
+
+  //Check if username already exists
+  while (existingUser != -1) {
+
+    //If username exists in array, generate a new one
+    currUsername = generateUsername();
+    existingUser = chatUsers.indexOf(currUsername);
+  }
+
+  //Store nickname of each user within socket itself
+  socket.nickname = currUsername;
+  chatUsers.push(socket.nickname);
+
+  //Emit the chat user's array to all the users so they can update their
+  //list when someone new joins
+
+  io.sockets.emit('usernames', chatUsers);
+
+  socket.on('chat', function(msg){
+
+    //We obtain times stamp for message emit below
+    let currDate = new Date();
+    let hours = currDate.getHours();
+    let minutes = currDate.getMinutes();
+    let seconds = currDate.getSeconds();
+
+    let time = hours + ":" + minutes + ":" + seconds;
+
+	   io.emit('chat', time + " "  + currUsername + ":" + " " + msg);
+  });
+
+   socket.on('disconnect', function(data) {
+
+     //Check if they have a nickname set first
+     if (!socket.nickname) return;
+
+     //User leaves chat, active users are updated
+     chatUsers.splice(chatUsers.indexOf(socket.nickname), 1);
+     io.sockets.emit('usernames', chatUsers);
+
+    })
 });
+
+/* Function creates random number's simulating GUIDs */
+function createUserID() {
+
+  //Takes the floor of a "randomly" generated number (Partially based on RFC4122)
+  let id = Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+
+  return id;
+
+}
+
+//Generate username
+function generateUsername() {
+
+  let nickName = "user" + createUserID();
+
+  return nickName;
+
+}
--- public/client.js
+++ public/client.js
@@ -1,12 +1,39 @@
 // shorthand for $(document).ready(...)
 $(function() {
+
+  var $nickname = $('#nicknames');
+  var $chatForm = $('#sendForm');
+
     var socket = io();
-    $('form').submit(function(){
-	socket.emit('chat', $('#m').val());
-	$('#m').val('');
-	return false;
+
+    $chatForm.submit(function(){
+
+	     socket.emit('chat', $('#m').val());
+	     $('#m').val('');
+	     return false;
+
     });
-    socket.on('chat', function(msg){
-	$('#messages').append($('<li>').text(msg));
+
+
+   socket.on('chat', function(msg){
+
+      $('#messages').append($('<li>').text(msg));
+
+   });
+
+    socket.on('usernames', function(data){
+
+      var currUser = "";
+
+      let i = 0;
+      for (i in data) {
+        currUser += data[i] + '<br/>';
+      }
+
+      $nickname.html(currUser);
+
     });
+
+
+
 });
